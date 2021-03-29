4e163a9fc1112f7a89d48bec1964fef321bec3a4
Kgebski <kgebski68@gmail.com>
2021-03-29 20:05:51 +0200

    basic networking game

---
+++ Network.gd
@@ -0,0 +1,36 @@
+extends Node
+
+
+const DefaultPort = 28960
+const MaxPlayers = 6
+
+var server = null
+var client = null
+
+var ipAdress = ""
+
+func _ready():
+	
+	for ip in IP.get_local_addresses():
+		if ip.begins_with("192.168.") and not ip.ends_with(".1"):
+			ipAdress = ip
+	
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+
+	
+func _connected_to_server() -> void:
+	print("Yes")
+	
+func _server_disconnected() -> void:
+	print("Yes but actually no")
+	
+func create_server() -> void:
+	server = NetworkedMultiplayerENet.new()
+	server.create_server(DefaultPort, MaxPlayers)
+	get_tree().set_network_peer(server)
+	
+func join_server() -> void:
+	client = NetworkedMultiplayerENet.new()
+	client.create_client(ipAdress, DefaultPort)
+	get_tree().set_network_peer(client)
---
+++ UI.gd
@@ -0,0 +1,49 @@
+extends Control
+
+var player = load("res://Player.tscn")
+
+onready var multiplayer_config_ui = $Multi_Conf
+onready var server_ip = $Multi_Conf/Server_ip_address
+
+onready var device_ip_address = $CanvasLayer/Devce_ip
+
+
+func _ready():
+	get_tree().connect("network_peer_connected",self, "_player_conected")
+	get_tree().connect("network_peer_disconnected",self, "_player_disconected")
+	get_tree().connect("connected_to_server",self, "_connected_to_server")
+	
+	device_ip_address.text = Network.ipAdress
+	
+func _player_conected(id):
+	print("Player " + str(id) + " has connected")
+	instance_player(id)
+	
+func _player_disconected(id):
+	print("Player " + str(id) + " has disconnected")
+	if Players.has_node(str(id)):
+		Players.get_node(str(id)).queue_free()
+		
+func _connected_to_server():
+	yield(get_tree().create_timer(0.1), "timeout")
+	instance_player(get_tree().get_network_unique_id())
+	
+func _on_Create_Server_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+	
+	instance_player(get_tree().get_network_unique_id())
+
+func _on_Join_Server_pressed():
+	if server_ip.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip = server_ip.text
+		Network.join_server() 
+		
+func instance_player(id) -> void:
+	var player_instance = Global.instance_node_at_location(player, Players, Vector2(rand_range(0,1920),rand_range(0,1080)))
+	player_instance.name = str(id)
+	player_instance.set_network_master(id)
+
+	
+	
